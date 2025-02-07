54a22863faf6de2a2e8c791441d8d1f4025fb57a
Christopher M. Sims <sesmar@gmail.com>
2025-02-07 16:40:32 -0500

    Getting Project up-to-date

--- GooniesScouting.ConsoleApp/obj/Debug/net9.0/GooniesScouting.ConsoleApp.AssemblyInfo.cs
+++ GooniesScouting.ConsoleApp/obj/Debug/net9.0/GooniesScouting.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GooniesScouting.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa4ca1eacb72d496695b7b4932fdeb23b35f4dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c911367ecf465f0862104487c9a80fa1a874355")]
 [assembly: System.Reflection.AssemblyProductAttribute("GooniesScouting.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GooniesScouting.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
